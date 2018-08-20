d7e0b2a8aeb6b96e474769ad3fb5355f72801dee
a.melnikau <a.melnikau@gmail.com>
2018-08-20 22:20:40 +0300

    added lines

--- GitTest.java
+++ GitTest.java
@@ -6,7 +6,9 @@ public class GitTest {
     public static void main(String[] args) {
 
         HashMap<Integer, String> hashMap = new HashMap<>();
-
+        hashMap.put(8, "Восемь");
+        hashMap.put(9, "Девять");
+        hashMap.put(10, "Десять");
     }
 }
 
