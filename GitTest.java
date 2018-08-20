489358b37a94b832210acd7797a298cf9ccdc4e2
a.melnikau <a.melnikau@gmail.com>
2018-08-20 22:13:52 +0300

    1

--- GitTest.java
+++ GitTest.java
@@ -1,16 +1,12 @@
 package gitTest;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class GitTest {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        HashMap<Integer, String> hashMap = new HashMap<>(3);
-            hashMap.put(sc.nextInt(), sc.next());
 
-        }
+        HashMap<Integer, String> hashMap = new HashMap<>();
+
     }
 }
 
